16380e7760d78af2bf1b4acf6c6488f5956315c4
Mayank Choudhary <7mayankchoudhary7@gmail.com>
2021-12-05 19:23:50 +0530

    Richest Customer Wealth(2D Array)

---
+++ src/main/java/binary_search/RichestCustomerWealth.java
@@ -0,0 +1,29 @@
+package binary_search;
+// question - https://leetcode.com/problems/richest-customer-wealth/
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts =  {{2,8,7},{7,1,3},{1,9,5}};
+        System.out.println(maximumWealth(accounts));
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        // for every person
+        if (accounts.length == 0) {
+            return 0;
+        }
+        int max = 0;
+        for (int person = 0; person < accounts.length; person++) {
+            int rowSum = 0;
+            // for every account of each person
+            for (int bankAccount = 0; bankAccount < accounts[person].length; bankAccount++) {
+                rowSum += accounts[person][bankAccount];
+            }
+            if (rowSum > max) {
+                max = rowSum;
+            }
+        }
+
+        return max;
+
+    }
+}
