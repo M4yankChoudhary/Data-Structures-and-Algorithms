f2bba0078c53c6ea20a6cc4ac1fbaf13b95f0e09
Mayank Choudhary <7mayankchoudhary7@gmail.com>
2021-12-05 18:41:34 +0530

    Search In 2D Array

---
+++ src/main/java/linear_search/SearchIn2DArray.java
@@ -0,0 +1,25 @@
+package linear_search;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int[][] arr = {{
+                1, 2, 3
+        }, {
+                4, 5, 6
+        }, {
+                7, 8, 9
+        }
+        };
+
+        int target = 54;
+
+
+        for(int row= 0; row < arr.length; row++){
+            for(int col =0; col < arr[row].length; col++) {
+                if(arr[row][col] == target){
+                    System.out.println(row+" "+col);
+                }
+            }
+        }
+    }
+}
